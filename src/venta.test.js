f79923973d69e17f60db7a92d2d099aa39ab2313
Sarah Cazorla <sarah.cazorla@ucb.edu.bo>
2026-03-09 01:32:21 -0400

    feat: Mostrar el porcentaje de impuesto que tiene ese estado

--- src/venta.test.js
+++ src/venta.test.js
@@ -1,9 +1,29 @@
-import  {MostrarPrecioNeto}  from "./venta.js"; 
+import { MostrarPrecioNeto, MostrarPorcentajeImpuesto, CalcularImpuesto, CalcularPrecioTotal } from "./venta.js";
 
 describe("Totalizador de Ventas", () => {
   
   it("debería calcular el total bruto multiplicando cantidad y precio", () => {
     expect(MostrarPrecioNeto(5, 10)).toEqual(50);
+  });
+
+  it("debería mostrar 8.25% de impuesto para CA", () => {
+    expect(MostrarPorcentajeImpuesto("CA")).toEqual("8.25%");
+  });
+
+  it("debería mostrar 6.65% de impuesto para UT", () => {
+    expect(MostrarPorcentajeImpuesto("UT")).toEqual("6.65%");
+  });
+
+  it("debería mostrar 8.00% de impuesto para NV", () => {
+    expect(MostrarPorcentajeImpuesto("NV")).toEqual("8.00%");
+  });
+
+  it("debería mostrar 6.25% de impuesto para TX", () => {
+    expect(MostrarPorcentajeImpuesto("TX")).toEqual("6.25%");
+  });
+
+  it("debería mostrar 4.00% de impuesto para AL", () => {
+    expect(MostrarPorcentajeImpuesto("AL")).toEqual("4.00%");
   });
 
 });
